a99c714c628c394c98cda12d4297c90f7019507e
Srihari Ruttala <45734841+SrihariRuttala@users.noreply.github.com>
2024-08-08 17:07:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from flask import Flask
 # current module (__name__) as argument.
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
